c478842780057861ff17138b93bae5786374ff53
AbdulAzizPhpDev <=>
2022-01-28 15:23:30 +0500

    update

--- app/addons/authentication/controllers/frontend/profiles.pre.php
+++ app/addons/authentication/controllers/frontend/profiles.pre.php
@@ -26,7 +26,28 @@ if (!defined('BOOTSTRAP')) {
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+
     if ($mode == 'send_sms') {
+
+        $pattern = "/^998\d{9}$/";
+
+        if (!preg_match($pattern, $_REQUEST['phone'])) {
+            $error = [
+                'status' => "error",
+                'response' => [
+                    "code" => "404",
+                    "message" => __('wrong_format_number'),
+                    "errors" => ""
+                ],
+                'data' => [
+                    'phone' => $_REQUEST['phone']
+                ]
+            ];
+
+            Registry::get('ajax')->assign('result', $error);
+            exit();
+        }
+
         $data = [
             'phone' => $_REQUEST['phone']
         ];
@@ -49,6 +70,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         $response_data = json_decode($response);
         curl_close($curl);
+
         $data = db_get_field('SELECT user_id FROM ?:users WHERE phone = ?i', $_REQUEST['phone']);
         if (empty($data)) {
             $user = create_user($_REQUEST['phone']);
@@ -66,54 +88,135 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             ];
             fn_set_session_data('user_info', $user_info);
         }
-        Registry::get('ajax')->assign('result', $response_data);
+        $error = [
+            'status' => "success",
+            'response' => [
+                "code" => "202",
+                "message" => __('massage_send'),
+                "errors" => ""
+            ],
+            'data' => [
+                'phone' => $_REQUEST['phone']
+            ]
+        ];
+
+        Registry::get('ajax')->assign('result', $error);
+        exit();
+    }
+
+
+    if ($mode == 'confirm') {
+
+        $pattern = "/^998\d{9}$/";
+
+        if (!preg_match($pattern, $_REQUEST['phone'])) {
+            $error = [
+                'status' => "error",
+                'response' => [
+                    "code" => "404",
+                    "message" => __('wrong_format_number'),
+                    "errors" => ""
+                ],
+                'data' => [
+                    'phone' => $_REQUEST['phone']
+                ]
+            ];
+
+            Registry::get('ajax')->assign('result', $error);
+            exit();
+        }
+        if (fn_get_session_data('user_info')['phone'] !== $_REQUEST['phone']) {
+            $error = [
+                'status' => "error",
+                'response' => [
+                    "code" => "404",
+                    "message" => __('wrong_phone_number'),
+                    "errors" => ""
+                ],
+                'data' => [
+                    'phone' => $_REQUEST['phone']
+                ]
+            ];
+
+            Registry::get('ajax')->assign('result', $error);
+            exit();
+        }
+
+        if (!preg_match($pattern, $_REQUEST['phone'])) {
+            $error = [
+                'status' => "error",
+                'response' => [
+                    "code" => "404",
+                    "message" => __('wrong_format_number'),
+                    "errors" => ""
+                ],
+                'data' => [
+                    'phone' => $_REQUEST['phone']
+                ]
+            ];
+
+            Registry::get('ajax')->assign('result', $error);
+            exit();
+        }
+
+        $data = [
+            "phone" => $_REQUEST['phone'],
+            "code" => $_REQUEST['code'],
+        ];
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://dev.paymart.uz/api/v1/login/auth',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => json_encode($data),
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        $response = json_decode($response);
+//
+//        data:
+//        access_token: "7dbe6bed62c8535daf18ae5b42b30ea1"
+//api_token: "7dbe6bed62c8535daf18ae5b42b30ea1"
+//user_id: 371618
+//user_status: 1
+//[[Prototype]]: Object
+//is_seller: 0
+//response: {code: 200, message: Array(0), errors: Array(0)}
+//status: "success"
+
+
+        if ($response->status == "success") {
+            $user_info['api_key'] = $response->data->access_token;
+            $user_info['p_user_id'] = $response->data->user_id;
+            db_query('UPDATE ?:users SET ?u WHERE user_id = ?i', $user_info, (int)fn_get_session_data('user_info')['id']);
+
+        }
+
+
+        Tygh::$app['session']->regenerateID();
+        fn_login_user(fn_get_session_data('user_info')['id'], true);
+
+        Helpdesk::auth();
+
+        if (!empty($_REQUEST['redirect_url'])) {
+            return $_REQUEST['redirect_url'];
+        }
+//        else {
+//            $redirect_url = fn_url('auth.login' . !empty($_REQUEST['return_url']) ? '?return_url=' . $_REQUEST['return_url'] : '');
+//        }
+
+        Registry::get('ajax')->assign('result', json_decode($response));
         exit();
 
     }
-
-
-}
-
-if ($mode == 'confirm') {
-
-    $data = [
-        "phone" => $_REQUEST['phone'],
-        "code" => $_REQUEST['code'],
-    ];
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://dev.paymart.uz/api/v1/login/auth',
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS => json_encode($data),
-        CURLOPT_HTTPHEADER => array(
-            'Content-Type: application/json'
-        ),
-    ));
-
-    $response = curl_exec($curl);
-
-    curl_close($curl);
-
-    Tygh::$app['session']->regenerateID();
-    fn_login_user(fn_get_session_data('user_info')['id'], true);
-
-    Helpdesk::auth();
-
-    if (!empty($_REQUEST['redirect_url'])) {
-        $redirect_url = $_REQUEST['redirect_url'];
-    } else {
-        $redirect_url = fn_url('auth.login' . !empty($_REQUEST['return_url']) ? '?return_url=' . $_REQUEST['return_url'] : '');
-    }
-
-    Registry::get('ajax')->assign('result', json_decode($response));
-    exit();
-
 }
 
