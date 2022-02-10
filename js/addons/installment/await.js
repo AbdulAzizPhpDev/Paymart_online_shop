8d4204f6c2dc23de42bade22dfd3c5d86c452a61
Azimjon Toirov <atoirov@paymart.uz>
2022-02-10 17:36:04 +0500

    await page almost done without animation button

--- js/addons/installment/await.js
+++ js/addons/installment/await.js
@@ -1,5 +1,3 @@
-import { fn_url } from '../../core/src/core/utils';
-
 (function (_, $) {
   const $lottie = $('.lottie-player');
   const $awaitBtn = $('#await-btn');
@@ -33,47 +31,26 @@ import { fn_url } from '../../core/src/core/utils';
       }
     },
     checkStatus: function () {
-      $.ceAjax('request', fn_url('installment_product.check_status'), {
-        method: 'POST',
-        callback: function (response) {
-          console.log(response);
-          /*if (response) {
+      const { baseUrl, api_token } = awaitState;
+
+      $.ajax({
+        url: `${baseUrl}/buyer/check_status`,
+        type: 'GET',
+        headers: {
+          Authorization: `Bearer ${api_token}`,
+        },
+        success: function (response) {
+          if (response) {
             if (response.status === 'success') {
-              switch (response.data.status) {
-                case 0:
-                  awaitMethods.makeRoute({ action: 'index' });
-                  break;
-                case 1:
-                  awaitMethods.makeRoute({ action: 'card-add' });
-                  break;
-                case 2:
-                case 6:
-                  awaitMethods.makeRoute({ action: 'await' });
-                  break;
-                case 4:
-                  awaitMethods.makeRoute({ action: 'contract-create' });
-                  break;
-                case 5:
-                case 10:
-                case 11:
-                  awaitMethods.makeRoute({ action: 'type-passport' });
-                  break;
-                case 12:
-                  awaitMethods.makeRoute({ action: 'guarant' });
-                  break;
-                case 8:
-                  awaitMethods.makeRoute({ action: 'refusal' });
-                  break;
-                default:
-                  console.error('User status not responded' + response.data.status);
-                  break;
+              if (Number(response.data.status) !== 2) {
+                window.location.reload();
               }
             } else {
-              awaitMethods.renderErrors(response.response.message);
+              awaitMethods.renderErrors(response.response.message)
             }
           } else {
-            console.error('Result does not exist. %cmethod[/buyer/detail] P.S. await-page', 'color: white; padding: 2px 5px; border: 1px dashed green');
-          }*/
+            console.error('Result does not exist. %cmethod[/buyer/check_status] P.S. await-page', 'color: white; padding: 2px 5px; border: 1px dashed green');
+          }
         },
       });
     },
