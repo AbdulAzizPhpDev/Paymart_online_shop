aa6fcf1662c2da2f9f3cd3778c9a3d45dd3b170a
AbdulAzizPhpDev <=>
2022-03-07 12:49:21 +0500

    changed

--- app/addons/installment/controllers/frontend/installment_product.php
+++ app/addons/installment/controllers/frontend/installment_product.php
@@ -640,7 +640,7 @@ if ($mode == "contract-create") {
 
         $items = $response->data->orders->$id->price;
 
-        $city = db_get_row('select * from ?:fargo_countries where parent_city_id=?i', 0);;
+        $city = db_get_array('select * from ?:fargo_countries where parent_city_id=?i', 0);;
 
         Tygh::$app['view']->assign('city', $city);
         Tygh::$app['view']->assign('total', $items->total);
