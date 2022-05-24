08ddcd4ac6c3412f155111f58611a6b42012f803
Azimjon Toirov <atoirov@paymart.uz>
2022-05-24 19:17:43 +0500

    show regions and cities in checkout page

--- design/themes/responsive/templates/addons/installment/overrides/views/checkout/components/customer/address.tpl
+++ design/themes/responsive/templates/addons/installment/overrides/views/checkout/components/customer/address.tpl
@@ -7,14 +7,23 @@
     {$element_identifier = "user-profiles"}
     {$group_meta = ""}
 {/if}
+{*{fn_print_die($cities)}*}
 
 <div class="{$group_meta} litecheckout__group" data-ca-lite-checkout-element="{$element_identifier}"
      data-ca-address-position="{$settings.Checkout.address_position}">
     {if $show_profiles_on_checkout}
         {include file="views/checkout/components/user_profiles.tpl"}
     {else}
+        <div class="alert alert-info delivery-date-container" style="padding: 10px 16px;" role="alert">
+            <span>{__('approximate_delivery_days')}</span>
+            <span class="delivery-date__days"></span>
+        </div>
+
         <div class="row-fluid" style="padding: 0 4px">
-            <div class="span8">
+            <div class="span6">
+                <input type="text" disabled value="Узбекистан" style="width: 100%;">
+            </div>
+            <div class="span5">
                 <select
                         class="cm-country cm-location-shipping"
                         style="width: 100%"
@@ -22,26 +31,22 @@
                         id="formAddress__select"
                 >
                     {foreach $cities as $value}
-                        <option selected value="{$value['city_id']}">{$value['city_name']}</option>
+                        <option value="{$value['id']}" data-delivery-days="{$value['days']}">{$value['city_name']}</option>
                     {/foreach}
                 </select>
             </div>
-            <div class="span8">
-                {*                <input name="user_data[region]" id="smth" type="text" style="width: 100%" placeholder="Район">*}
-
-                <select name="user_data[distract]" id="formAddress5" class="tashkent-regions d-none">
-                </select>
-                <input type="text" placeholder="район" class="not-tashkent-region">
+            <div class="span5">
+                <select name="user_data[distract]" id="formAddress5" class="city-regions" style="width: 100%;"></select>
             </div>
         </div>
         <div class="row-fluid" style="padding: 8px 4px 0 4px">
-            <div class="span8">
+            <div class="span6">
                 <input name="user_data[street]" type="text" style="width: 100%" placeholder="Улица">
             </div>
-            <div class="span4">
+            <div class="span5">
                 <input name="user_data[home]" type="text" style="width: 100%" placeholder="Дом">
             </div>
-            <div class="span4">
+            <div class="span5">
                 <input name="user_data[apartment]" type="text" style="width: 100%" placeholder="Квартира">
             </div>
         </div>
--- js/addons/installment/adress.js
+++ js/addons/installment/adress.js
@@ -1,28 +1,73 @@
-const $select = $('.tashkent-regions');
-const $input = $('.not-tashkent-region');
+(function (_, $) {
+  const $regions = $('.city-regions');
+  const defaultSelectedCity = $('#formAddress__select :selected').val();
+  const $deliveryDateContainer = $('.delivery-date__days');
 
-$(document).ready(function () {
-  $('#formAddress__select').change(function (e) {
-    // var selectedOptionAdress = formAdressSelect.options[formAdressSelect.selectedIndex].value;
-    const city_id = e.target.value;
-    $.ceAjax('request', fn_url('get_city.city'), {
-      method: 'post',
-      data: {
-        city_id: city_id,
-      },
-      callback: function (response) {
-        if (response.result == null) {
-          $input.removeClass('d-none').focus();
-          $select.addClass('d-none');
-        } else {
-          $select.removeClass('d-none');
-          $input.addClass('d-none');
+  const addressState = {
+    deliveryDays: 2,
+    totalDeliveryDays: 0,
+  };
+
+  const addressMethods = {
+    calculateDeliveryDate: function (deliveryDay, extraDay = 0) {
+      const now = new Date();
+      const deliveryDate = now.addDays(deliveryDay + extraDay);
+
+      addressState.totalDeliveryDays = deliveryDay + extraDay;
+
+      const extraDeliveryDate = deliveryDate.addDays(2).toLocaleDateString();
+      const labelDelivery = `${deliveryDate.toLocaleDateString()} - ${extraDeliveryDate}`;
+
+      $deliveryDateContainer.text(labelDelivery);
+
+    },
+    getRegions: function (cityId = null) {
+      $.ceAjax('request', fn_url('get_city.city'), {
+        method: 'post',
+        data: {
+          city_id: cityId || defaultSelectedCity,
+        },
+        callback: function (response) {
+          if (response.result == null) {
+            alert('response null');
+            return false;
+          }
+
+          addressMethods.calculateDeliveryDate(addressState.deliveryDays, 0);
+
+          $regions.empty();
           response.result.forEach(number => {
-            const option = `<option value="${number.city_id}" selected>${number.city_name}</option>`;
-            $select.append(option);
+            const option = `<option value="${number.city_id}" data-extra-days="${number.extra_days}">${number.city_name}</option>`;
+            $regions.append(option);
           });
-        }
-      },
-    });
-  });
-});
+        },
+      });
+    },
+    getRegionsByCityId: function (event) {
+      const selectedCityId = event.target.value;
+      addressState.deliveryDays = $(this).find(':selected').data('delivery-days') || 0;
+
+      addressMethods.calculateDeliveryDate(addressState.deliveryDays, 0);
+
+      addressMethods.getRegions(selectedCityId);
+    },
+    onSelectRegion: function () {
+      const extraDays = $(this).find(':selected').data('extra-days');
+
+      addressMethods.calculateDeliveryDate(addressState.deliveryDays, extraDays);
+    },
+  };
+
+  $('#formAddress__select').on('change', addressMethods.getRegionsByCityId);
+
+  $regions.on('change', addressMethods.onSelectRegion);
+
+  addressMethods.getRegions();
+
+  Date.prototype.addDays = function (days) {
+    const date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+  };
+
+})(Tygh, Tygh.$);
